688fafd2a764e4413043f7b7261190c4610f3f63
noelav07 <noel.a.v.07@gmail.com>
2024-07-13 18:50:47 +0530

    task_5

--- task5/task5.sh
+++ task5/task5.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-
+groupdel ims_rp29 && groupdel linux
 groupadd linux
 getent group linux
-useradd gold
-useradd ben
-useradd -G linux -p redhat 
-useradd -G linux centos
-useradd -s /sbin/nologin ubuntu 
+useradd gold && echo "created user:gold"
+useradd ben && echo "created user:ben"
+useradd -G linux redhat && echo "created user:redhat"
+useradd -G linux centos && echo "created user:centos"
+useradd -s /sbin/nologin ubuntu && echo "created user:ubuntu"
 pd=kgM@123
 for names in redhat centos ubuntu;
 do
     echo "$names:$pd"| chpasswd
-    echo "created $(names)" 
+    echo "created $names" 
 done;
 useradd -u 6000 sam
-id -u centos && sudo usermod -u 10001 centos ; echo "centos:$($(id -u centos))"
-dnf install fish > /dev/null
-su - ubuntu -c 'echo $0' && usermod -s /bin/fish
-usermod -l redhat 
-su - redhat 
-groupadd ims_rp29
-
-for i in noel{1..5};
-do 
-    gpasswd $(i) ims_rp29
+id -u centos && sudo usermod -u 10001 centos ; echo "centos:$(id -u centos)"
+dnf install fish -y > /dev/null
+chsh -s /bin/fish ubuntu
+usermod -L redhat 
+groupadd ims_rp29 
+for i in noel{1..5};do 
+useradd $i;
+gpasswd -a $i ims_rp29 
 done;
+echo -e "details of the added users: \n\n"
+tail /etc/passwd | cat -n 
+
+# for users in ubuntu redhat gold ben centos sam ; do userdel -r $users ; done
+# for noels in noel{1..5}; do userdel -r $noels; done
