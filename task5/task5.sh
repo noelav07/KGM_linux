e6db517b2b708b07ce3967d3172668b14ee45212
noelav07 <noel.a.v.07@gmail.com>
2024-07-14 17:39:55 +0530

    minor_updates

--- task5/task5.sh
+++ task5/task5.sh
@@ -8,11 +8,7 @@ useradd -G linux redhat && echo "created user:redhat"
 useradd -G linux centos && echo "created user:centos"
 useradd -s /sbin/nologin ubuntu && echo "created user:ubuntu"
 pd=kgM@123
-for names in redhat centos ubuntu;
-do
-    echo "$names:$pd"| chpasswd
-    echo "created $names" 
-done;
+for names in redhat centos ubuntu; do echo "$names:$pd"| chpasswd; echo "created $names"; done;
 useradd -u 6000 sam
 id -u centos && sudo usermod -u 10001 centos ; echo "centos:$(id -u centos)"
 dnf install fish -y > /dev/null
